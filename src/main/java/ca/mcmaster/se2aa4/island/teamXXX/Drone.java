833d1f609cde1bb9a14caa472781a66d0cb82f16
Kelly Deng <kd0819@Kellys-MacBook-Air.local>
2025-03-13 15:33:52 -0400

    drone status check

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
@@ -1,38 +1,117 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
-class Drone {
-    private int batteryLevel;
-    private String heading;
-    private final Radar radar; // Composition: Drone has a Radar
-    private final Photo photo; // Composition: Drone has a photo scanner
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.json.*;
+import java.io.StringReader;
 
-    public Drone(int batteryLevel, String heading) {
-        this.batteryLevel = batteryLevel;
-        this.heading = heading;
-        this.radar = new Radar(); // The Drone creates its Radar instance
-        this.photo = new Photo(); 
+import eu.ace_design.island.game.actions.Heading;
+
+import java.util.ArrayList;
+
+public class Drone {
+
+    private final Logger log = LogManager.getLogger();
+    private Integer energyReserve = 0;
+    private Direction currentHeading = Direction.NORTH;
+    private String discoveredLocation;
+    private Integer scanRange;
+    private Boolean isOverWater = false;
+    private Boolean creekDetected = false;
+    private Boolean siteDetected = false;
+    private Boolean islandInView = false;
+    public String creekID = "";
+    public String siteID = "";
+
+    /*
+    Initializes the Drone's energy reserve and heading.
+    */
+    public void initializeDrone(String jsonInput) {
+        JSONObject droneData = new JSONObject(new JSONTokener(new StringReader(jsonInput)));
+        energyReserve = droneData.getInt("budget");
+        updateHeading(droneData);
     }
 
-    public int getBatteryLevel() {
-        return batteryLevel;
+    /*
+    Updates drone status using the provided JSON response.
+    */
+    public void refreshDroneStatus(String jsonResponse) {
+        JSONObject response = new JSONObject(new JSONTokener(new StringReader(jsonResponse)));
+        log.info(response);
+
+        //Battery check
+        if (energyReserve < response.getInt("cost")) {
+            log.warn("Insufficient energy for this action.");
+        } else {
+            energyReserve -= response.getInt("cost");
+        }
+
+        if (response.has("extras")) {
+            JSONObject extras = response.getJSONObject("extras");
+            if (extras.has("found")) {
+                discoveredLocation = extras.getString("found");
+
+            }
+
+            if (extras.has("biomes")) {
+                JSONArray biomesData = extras.getJSONArray("biomes");
+                ArrayList<String> biomesList = new ArrayList<>();
+                for (int i = 0; i < biomesData.length(); i++) {
+                    biomesList.add(String.valueOf(biomesData.get(i)));
+                }
+                this.isOverWater = biomesList.contains("OCEAN");
+            }
+
+            if (extras.has("range")) {
+                scanRange = extras.getInt("range");
+                log.info("Updated scan range: " + scanRange);
+            }
+
+            if (extras.has("creeks")) {
+                JSONArray creeksData = extras.getJSONArray("creeks");
+
+                if (!creeksData.isEmpty()) {
+                    for (int i = 0; i < creeksData.length(); i++) {
+                        creekID = creeksData.get(i).toString();
+                        creekDetected = true;
+                    }
+                }
+            }
+
+            if (extras.has("sites")) {
+                JSONArray sitesData = extras.getJSONArray("sites");
+
+                if (!sitesData.isEmpty()) {
+                    for (int i = 0; i < sitesData.length(); i++) {
+                        siteID = sitesData.get(i).toString();
+                        siteDetected = true;
+                    }
+                }
+            }
+        }
+        updateHeading(response);
     }
 
-    public void fly() {}
-
-    public void turnLeft() {}
-
-    public void turnRight() {}
-
-    public void stop(){}
 
 
-    public String scanWithRadar(){
-        String radarResult = radar.RadarScan(heading);
-        return radarResult;
+    /*
+    Updates the drone’s heading based on response data.
+    */
+    private void updateHeading(JSONObject response) {
+        if (response.has("heading")) {
+            String newHeading = response.getString("heading");
+            log.info("New heading detected: " + newHeading);
+            currentHeading = currentHeading.getHeadingFromString(newHeading);
+            log.info("Updated heading: " + currentHeading);
+        }
     }
 
-    public String scanWithPhoto(){
-        String photoResult = photo.PhotoScan();
-        return photoResult;
-    }
+
+    /*
+     Getters and Setters
+     */
+
 }
